ba247b0d9301e3865b0288cf6eaada6207f1ce16
derobi <Derobi@live.com>
2016-12-23 16:03:27 -0600

    added apnonce support

--- SaveBlobs.sh
+++ SaveBlobs.sh
@@ -3,18 +3,24 @@
 #  SaveBlobs.sh
 #created by @if0xxx
 #save em all
+
 saveblobs(){
 	./Resources/tsschecker* -d "$idevice_id" -e "$idevice_ecid" -i "$version" -s 
 }
 saveblobswithbuild(){
 	./Resources/tsschecker* -d "$idevice_id" -e "$idevice_ecid" -i "$version" --buildid "$build" -s 
 }
+
+saveblobswithapnonce(){
+	./Resources/tsschecker* -d "$idevice_id" -e "$idevice_ecid" -i "$version" -s --apnonce "$apnonce" 
+}
 main(){
-	echo " For wich iOS Vesion you want save Blobs?"
+	echo " For which iOS version you want save Blobs?"
 	read version
 	echo "any specific build? if not just hit enter"
 	read build
-}
+	echo "Enter the apnonce you want to save with"
+	read apnonce
 run(){
 	while [ $i -le $idevice_number ]
     do  count=$(( $count+1 ))
@@ -27,10 +33,16 @@ run(){
 				then saveblobs
 				mkdir -p ./Blobs/"$idevice_name1"/"$version"
 				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			else 	
 				saveblobswithbuild
 				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$build"
 				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$build"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			fi
         elif [ $count = 2 ]
             then idevice_id="$idevice_id2"
@@ -39,10 +51,16 @@ run(){
 				then saveblobs
 				mkdir -p ./Blobs/"$idevice_name2"/"$version"
 				mv ./*.shsh2 ./Blobs/"$idevice_name2"/"$version"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			else 	
 				saveblobswithbuild
 				mkdir -p ./Blobs/"$idevice_name2"/"$version"/"$build"
 				mv ./*.shsh2 ./Blobs/"$idevice_name2"/"$version"/"$build"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			fi
         elif [ $count = 3 ]
             then idevice_id="$idevice_id3"
@@ -51,10 +69,16 @@ run(){
 				then saveblobs
 				mkdir -p ./Blobs/"$idevice_name3"/"$version"
 				mv ./*.shsh2 ./Blobs/"$idevice_name3"/"$version"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			else 	
 				saveblobswithbuild
 				mkdir -p ./Blobs/"$idevice_name3"/"$version"/"$build"
 				mv ./*.shsh2 ./Blobs/"$idevice_name3"/"$version"/"$build"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			fi
         elif [ $count = 4 ]
             then ridevice_id="$idevice_id4"
@@ -63,10 +87,16 @@ run(){
 				then saveblobs
 				mkdir -p ./Blobs/"$idevice_name4"/"$version"
 				mv ./*.shsh2 ./Blobs/"$idevice_name4"/"$version"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			else 	
 				saveblobswithbuild
 				mkdir -p ./Blobs/"$idevice_name4"/"$version"/"$build"
 				mv ./*.shsh2 ./Blobs/"$idevice_name4"/"$version"/"$build"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			fi
         elif [ $count = 5 ]
             then idevice_id="$idevice_id5"
@@ -75,10 +105,16 @@ run(){
 				then saveblobs
 				mkdir -p ./Blobs/"$idevice_name5"/"$version"
 				mv ./*.shsh2 ./Blobs/"$idevice_name5"/"$version"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			else 	
 				saveblobswithbuild
 				mkdir -p ./Blobs/"$idevice_name5"/"$version"/"$build"
 				mv ./*.shsh2 ./Blobs/"$idevice_name5"/"$version"/"$build"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			fi
         elif [ $count = 6 ]
             then idevice_id="$idevice_id6"
@@ -87,10 +123,16 @@ run(){
 				then saveblobs
 				mkdir -p ./Blobs/"$idevice_name6"/"$version"
 				mv ./*.shsh2 ./Blobs/"$idevice_name6"/"$version"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			else 	
 				saveblobswithbuild
 				mkdir -p ./Blobs/"$idevice_name6"/"$version"/"$build"
 				mv ./*.shsh2 ./Blobs/"$idevice_name6"/"$version"/"$build"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			fi
         elif [ $count = 7 ]
             then idevice_id="$idevice_id7"
@@ -99,10 +141,16 @@ run(){
 				then saveblobs
 				mkdir -p ./Blobs/"$idevice_name7"/"$version"
 				mv ./*.shsh2 ./Blobs/"$idevice_name7"/"$version"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			else 	
 				saveblobswithbuild
 				mkdir -p ./Blobs/"$idevice_name7"/"$version"/"$build"
 				mv ./*.shsh2 ./Blobs/"$idevice_name7"/"$version"/"$build"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			fi
         elif [ $count = 8 ]
             then idevice_id="$idevice_id8"
@@ -111,10 +159,16 @@ run(){
 				then saveblobs
 				mkdir -p ./Blobs/"$idevice_name8"/"$version"
 				mv ./*.shsh2 ./Blobs/"$idevice_name8"/"$version"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			else 	
 				saveblobswithbuild
 				mkdir -p ./Blobs/"$idevice_name8"/"$version"/"$build"
 				mv ./*.shsh2 ./Blobs/"$idevice_name8"/"$version"/"$build"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			fi
         elif [ $count = 9 ]
             then idevice_id="$idevice_id9"
@@ -123,10 +177,16 @@ run(){
 				then saveblobs
 				mkdir -p ./Blobs/"$idevice_name9"/"$version"
 				mv ./*.shsh2 ./Blobs/"$idevice_name9"/"$version"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			else 	
 				saveblobswithbuild
 				mkdir -p ./Blobs/"$idevice_name9"/"$version"/"$build"
 				mv ./*.shsh2 ./Blobs/"$idevice_name9"/"$version"/"$build"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			fi
         elif [ $count = 10 ]
             then idevice_id="$idevice_id10"
@@ -135,13 +195,21 @@ run(){
 				then saveblobs
 				mkdir -p ./Blobs/"$idevice_name10"/"$version"
 				mv ./*.shsh2 ./Blobs/"$idevice_name10"/"$version"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			else 	
 				saveblobswithbuild
 				mkdir -p ./Blobs/"$idevice_name10"/"$version"/"$build"
 				mv ./*.shsh2 ./Blobs/"$idevice_name10"/"$version"/"$build"
+				saveblobswithapnonce
+				mkdir -p ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
+				mv ./*.shsh2 ./Blobs/"$idevice_name1"/"$version"/"$apnonce"
 			fi
         fi
+   
     done
+
 }
 i=1
 count=0
